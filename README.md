ca71e3037033c8f956ed4fed59a1efa2999ad81a
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-02-22 18:51:06 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-CMakeUtilitiesTemplate
-======================
+JSON
+====
--- tests/tests.cmake
+++
@@ -1,8 +0,0 @@
-target_link_libraries(${TARGET_NAME} PRIVATE solver)
-target_link_libraries(${TARGET_NAME} PRIVATE thread_pool)
-target_link_libraries(${TARGET_NAME} PRIVATE online_scheduling)
-
-if (MSVC)
-    # ensure __VA_OPT__ is supported (C++20 feature)
-    target_compile_options(${TARGET_NAME} PRIVATE "/Zc:preprocessor")
-endif ()
