98f05514c6b68bc6e470f74e3045143dd2285f56
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-02-22 18:48:44 +0100

    init

--- json/executable.cmake
+++ json/executable.cmake
@@ -1,16 +1,6 @@
-execute_python("${TARGET_DIR}/generator.py")
-
 if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     set_target_properties(${TARGET_NAME} PROPERTIES INTERPROCEDURAL_OPTIMIZATION ON)
-    target_compile_options(
-            ${TARGET_NAME} PRIVATE
-            -O3
-            -flto
-            -march=native
-            -funroll-loops
-            -finline-functions
-            -foptimize-sibling-calls
-            -fno-exceptions
-            -fno-rtti
-    )
+    target_compile_options(${TARGET_NAME} PRIVATE -O3 -flto -march=native -funroll-loops -finline-functions -foptimize-sibling-calls -fno-exceptions -fno-rtti)
 endif ()
+
+python("${TARGET_DIR}/generator.py")
